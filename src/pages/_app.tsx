c83a73922445869281f861a5a37e321ddb9018d2
luongdao <luong.dao@pionero.io>
2023-06-20 11:02:11 +0700

    setup trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "swiper/css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { trpc } from "@/libs/trpc";
 
 // For pages with nested layouts
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -13,8 +14,10 @@ export type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
 
   return <>{getLayout(<Component {...pageProps} />)}</>;
 }
+
+export default trpc.withTRPC(App);
