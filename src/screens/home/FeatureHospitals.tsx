6f0f2c8a4098f7a07215f81c481ec485438d11c8
tiennm <nmtienptg@gmail.com>
2023-06-20 16:48:53 +0700

    update background

--- src/screens/home/FeatureHospitals.tsx
+++ src/screens/home/FeatureHospitals.tsx
@@ -35,49 +35,51 @@ const hospitals = [
 
 const FeatureHospitals: FC<FeatureHospitalsProps> = ({}) => {
   return (
-    <div className="mt-10 w-full max-w-[1140px] relative mx-auto">
-      <h3 className="text-center text-2xl mb-8 uppercase">
-        hệ thống bệnh viện triển khai
-      </h3>
+    <div className="w-full bg-[#F8FAFD] pb-16">
+      <div className="mt-10 w-full max-w-[1140px] relative mx-auto">
+        <h3 className="text-center text-2xl mb-8 uppercase">
+          hệ thống bệnh viện triển khai
+        </h3>
 
-      <div className="w-full relative">
-        <button className="absolute top-1/2 -translate-y-[25px] -left-10 text-2xl">
-          <FaChevronCircleLeft />
-        </button>
-        <button className="absolute top-1/2 -translate-y-[25px] -right-10 text-2xl">
-          <FaChevronCircleRight />
-        </button>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-          className="mySwiper"
-          modules={[Autoplay]}
-          speed={600}
-        >
-          {hospitals.map((hospital, index) => (
-            <SwiperSlide
-              key={index}
-              className="flex flex-col gap-4 items-center"
-            >
-              <a className="block w-full" href={hospital.href}>
-                <img
-                  src={hospital.image}
-                  className="w-full object-cover rounded-lg"
-                  alt=""
-                />
+        <div className="w-full relative">
+          <button className="absolute top-1/2 -translate-y-[25px] -left-10 text-2xl">
+            <FaChevronCircleLeft />
+          </button>
+          <button className="absolute top-1/2 -translate-y-[25px] -right-10 text-2xl">
+            <FaChevronCircleRight />
+          </button>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            autoplay={{
+              delay: 1000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            className="mySwiper"
+            modules={[Autoplay]}
+            speed={600}
+          >
+            {hospitals.map((hospital, index) => (
+              <SwiperSlide
+                key={index}
+                className="flex flex-col gap-4 items-center"
+              >
+                <a className="block w-full" href={hospital.href}>
+                  <img
+                    src={hospital.image}
+                    className="w-full object-cover rounded-lg"
+                    alt=""
+                  />
 
-                <h3 className="text-center uppercase mt-4 text-lg">
-                  {hospital.name}
-                </h3>
-              </a>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+                  <h3 className="text-center uppercase mt-4 text-lg">
+                    {hospital.name}
+                  </h3>
+                </a>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
